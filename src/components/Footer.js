1a56684f5410a9d84c174dc7f40310856ec1f3a7
Hanzla Haroon <hanzla.haroon@outlook.com>
2023-01-31 13:58:43 +0500

    fix: removed self-calling component from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <>
-    <Footer className='py-3'></Footer>
-    <Footer className='py-3'></Footer>
-    <Footer className='py-3'>
-    <div className='container-xxl'>
-      <div className='row'>
-        <div className='col-12'>
-        <h1>THIS IS FOOTER</h1>
+      <footer className='py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <h1>THIS IS FOOTER</h1>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    </Footer>
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
