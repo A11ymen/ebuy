cf35a804927fb95aa363c44d87a777181ee82f3f
Hanzla Haroon <hanzla.haroon@outlook.com>
2023-01-31 14:04:14 +0500

    fix: fixed ReactDOM import issue & on strict mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header'; 
-import Home from './pages/Home';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/signin' element={<Signin/>}/>
+        <Route index element={<Home />} />
+        <Route path='signup' element={<Signup />} />
+        <Route path='signin' element={<Signin />} />
       </Routes>
-      <Footer/>
-   </Router>
+      <Footer />
+    </Router>
   );
 }
 
