8b9182a829902777b0b1aa7c2dbc4fd0b5b3a831
Aymen <aymenhameed777@gmail.com>
2023-02-01 20:31:53 +0500

    dev created

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,78 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {BsInstagram,BsFacebook,BsTwitter} from 'react-icons/bs'
 
 const Footer = () => {
   return (
     <>
-      <footer className='py-3'>
+      <footer className='py-0'>
+        <div className='container-xl'>
+          <div className='row'>
+            <div className='col-4 '>
+              <h4 className='text-white mb-4'>Customer Care</h4>
+              
+              <div className='footer-links d-flex flex-column'>
+                <Link className='text-white py-2 mb-1'>How to buy</Link>
+                <Link className='text-white py-2 mb-1'>Became a seller</Link>
+                <Link className='text-white py-2 mb-1' >FAQs</Link>
+                <Link className='text-white py-2 mb-1'>Contact us</Link>
+              </div> 
+
+              <div className='social_icons '></div>
+              <a className='flex-row ms-2 text-white '  href="www.facebook.com"> 
+             <BsFacebook />
+              </a>
+              <a className='flex-row ms-2 text-white '  href="www.instagram.com"> 
+                <BsInstagram />
+              </a>
+              <a className='flex-row ms-2 text-white '  href="www.twitter.com">
+                <BsTwitter/>
+              </a>
+            </div>
+
+            <div className='col-3 ' >
+              <h4 className='text-white mb-4'>Information</h4>
+              <div className='footer-links d-flex flex-column'>
+                <Link className='text-white py-2 mb-1'>Privacy policy</Link>
+                <Link className='text-white py-2 mb-1'>Refund policy</Link>
+                <Link className='text-white py-2 mb-1' >Shipping policy</Link>
+                <Link className='text-white py-2 mb-1'>Returns & Refunds</Link>
+              </div>
+        
+            </div>
+
+            <div className='col-3 '>
+              <h4 className='text-white mb-4' >Account</h4>
+              <div className='footer-links d-flex flex-column'>
+                <Link className='text-white py-2 mb-1'>Home</Link>
+                <Link className='text-white py-2 mb-1'>About us</Link>
+                <Link className='text-white py-2 mb-1' >Terms & conditions</Link>
+              </div>
+        
+            </div>
+
+            <div className='col-2 '>
+              <h4 className='text-white mb-4' >Quick Links</h4>
+              <div className='footer-links d-flex flex-column'>
+                <Link className='text-white py-2 mb-1'>Electronics</Link>
+                <Link className='text-white py-2 mb-1'>Sports</Link>
+                <Link className='text-white py-2 mb-1' >Accessories</Link>
+                <Link className='text-white py-2 mb-1'>Home & Lifestyle</Link>
+              </div>
+        
+            </div>
+          </div>
+        </div>
+</footer>
+
+
+
+    {/* this is another footer @2023 powered by ebuy */}
+<footer className='py-0'>
         <div className='container-xxl'>
           <div className='row'>
             <div className='col-12'>
-              <h1>THIS IS FOOTER</h1>
+              <p className='text-center mb-0 text-white'>&copy; {new Date().getFullYear()} powered by E-buy</p>
             </div>
           </div>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,74 @@
 import React, { useEffect } from "react";
-
+import { Carousel } from "react-bootstrap";
 const Home = () => {
   useEffect(() => {
     console.log("Home Page");
   }, []);
 
   return (
+    
     <>
-      <h2>Home</h2>
+    <section classname='home-wrapper-1 py-5'>
+      <Carousel fade>
+      <Carousel.Item>
+        <img className="img-fluid d-block w-100" src={'/images/car1.jpg'}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="img-fluid d-block w-100"
+          src={'/images/women-cloth-main.jpg'}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="img-fluid d-block w-100"
+          src={'/images/mobile-main.jpg'}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </section>
+    <section className='home-wrapper-2 py-5'>
+      <div className='container.xxl'>
+        <div className='row'>
+          <div className='col-12'>
+            <div className='services d-flex align-items center justify-content-between'>
+              <div>
+                <img src={'images/service.png'} alt="services"></img>
+                <div>
+                  <h6>Free shipping</h6>
+                  <p>for all orders over 2500</p>
+                </div>
+              </div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
     </>
   );
 };
